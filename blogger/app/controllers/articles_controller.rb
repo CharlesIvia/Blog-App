064a9b704abf647442e5c1a65d9c44a724c441b8
Charles Ivia <iviacharles@gmail.com>
2020-03-19 12:47:19 +0300

    Create new article commit

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -5,5 +5,16 @@ class ArticlesController < ApplicationController
     def index 
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
    
+    def create
+        @article = Article.new
+        @article.title = params[:article][:title]
+        @article.body = params[:article][:body]
+        @article.save
+        redirect_to article_path(@article)      
+    end
 end
